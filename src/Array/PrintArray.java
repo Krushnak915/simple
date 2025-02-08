af78a952a92661ecff618391e753da289bfb6f6b
Krushnak915 <admin@DESKTOP-D9I58S8.bwrouter>
2025-02-08 14:27:05 +0530

    adding stream

--- src/Array/PrintArray.java
+++ src/Array/PrintArray.java
@@ -3,23 +3,22 @@ package Array;
 public class PrintArray {
 
 	public static void main(String[] args) {
-		int a[]= {1,2,5,2,5,3,8,3,6,9};
-		int b[]=new int[a.length];
-		for(int i=0;i<a.length;i++) {
-		
-			b[i]=a[i];
+		int a[] = { 1, 2, 5, 2, 5, 3, 8, 3, 6, 9 };
+		int b[] = new int[a.length];
+		for (int i = 0; i < a.length; i++) {
+
+			b[i] = a[i];
 		}
-		
+
 		System.out.println("original array");
-		for(int i=0;i<a.length;i++) {
-			System.out.print(a[i]+" ");
-			}
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
 		System.out.println();
-		
+
 		System.out.println("copy array");
-		for(int i=0;i<a.length;i++) {
-		System.out.print(b[i]+" ");
-	      }
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(b[i] + " ");
 		}
 	}
-
+}
